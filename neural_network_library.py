9cb36570f5c4e4e2eb1d01aa8689214f1388b51f
Edgar Chavez <ed5chavez5@gmail.com>
2025-02-16 13:47:45 -0600

    Tanh Class
    Added the Tanh class to solve the XOR problem and removed some debugging statements.

--- neural_network_library.py
+++ neural_network_library.py
@@ -12,7 +12,7 @@ class Layer():
     def forward(self, x):
         pass
 
-    def backward(self, grad_z):
+    def backward(self, grad):
         pass
 
 
@@ -31,28 +31,13 @@ class Linear(Layer):
         super().__init__()
 
         # Initialize weights and bias
-        self.weights = np.random.randn(output_size, input_size) #* 0.01
+        self.weights = np.random.randn(output_size, input_size) * 0.1
         self.bias = np.zeros(output_size) ##np.random.randn(output_size)
         self.x = None
-
-        ## debug print
-        ##print('weights should be init:', self.weights)
-        ##print('shape of weights:', self.weights.shape)
-
-        ##print('bias should be init:', self.bias)
-        ##print('shape of bias:', self.bias.shape)
 
         # set gradients to zero
         self.grad_weights = np.zeros_like(self.weights)
         self.grad_bias = np.zeros_like(self.bias)
-
-        ## debug print
-        ##print('grad_weights should be zeros:', self.grad_weights)
-        ##print('shape of grad_weights:', self.grad_weights.shape)
-
-        ##print('grad_bias should be zeros:', self.grad_bias)
-        ##print('shape of grad_bias:', self.grad_bias.shape)
-        
 
     def forward(self, x):
         """
@@ -64,61 +49,33 @@ class Linear(Layer):
             The input data. The shape of x is (batch_size, input_size)
         
         """
-        self.x = x
+        self.x = x # cache the input data for backpropagation
 
-        ## debug print
-        ##print('x:', x)
-        ##print('shape of x: ', x.shape)
-
-        z = np.dot(x, self.weights.T) + self.bias
-
-        ## debug print
-        ##print('z:', z)
-        ##print('shape of z:', z.shape)
-
-        return z
+        return np.dot(x, self.weights.T) + self.bias
     
-    def backward(self, grad_z):
+    def backward(self, grad):
         """
         Computes the backward pass of the linear layer.
         
         Parameters:
         ----------
-        grad_z: np.ndarray
+        grad: np.ndarray
             The gradient of the loss with respect to the output of the linear layer. The shape of grad_z is (batch_size, output_size)
         """
 
-        # Compute the gradients via the chain rule 
-        # derivative of loss with respect to weights = derivative of loss with respect to z * derivative of z with respect to weights
-        # derivative of loss with respect to bias = derivative of loss with respect to z * derivative of z with respect to bias
-        # derivative of loss with respect to x = derivative of loss with respect to z * derivative of z with respect to x
-        self.grad_weights = np.dot(grad_z.T, self.x)
-
-        ## debug print
-        ##print('grad_weights:', self.grad_weights)
-        ##print('shape of grad_weights:', self.grad_weights.shape)
-
-        self.grad_bias = np.sum(grad_z, axis=0)
+        # Compute the gradients for weights and bias
+        self.grad_weights = np.dot(grad.T, self.x)
+        self.grad_bias = np.sum(grad, axis=0)
+    
+        return np.dot(grad, self.weights)
         
-        ## debug print
-        ##print('grad_bias:', self.grad_bias)
-        ##print('shape of grad_bias:', self.grad_bias.shape)
-        
-        grad_x = np.dot(grad_z, self.weights)
-
-        ## debug print
-        ##print('grad_x:', grad_x)
-        ##print('shape of grad_x:', grad_x.shape)
-
-        return grad_x        
-
 class Sigmoid(Layer):
     def __init__(self):
         """
         Initializes a sigmoid layer.
         """
         super().__init__()
-        self.sigmoid = None
+        self.x = None
 
     def forward(self, x):
         """
@@ -129,8 +86,8 @@ class Sigmoid(Layer):
         x: np.ndarray
             The input data. The shape of x is (batch_size, input_size)
         """
-        self.sigmoid = 1 / (1 + np.exp(-x))
-        return self.sigmoid
+        self.x = 1 / (1 + np.exp(-x))
+        return self.x
 
     def backward(self, grad):
         """
@@ -141,8 +98,38 @@ class Sigmoid(Layer):
         grad: np.ndarray
             The gradient of the loss with respect to the output of the sigmoid layer. The shape of x is (batch_size, input_size)
         """
-        grad_x = grad * self.sigmoid * (1 - self.sigmoid)
-        return grad_x
+        return grad * self.x * (1 - self.x)
+
+class Tanh(Layer):
+    def __init__(self):
+        """
+        Initializes a tanh layer.
+        """
+        super().__init__()
+        self.x = None
+    
+    def forward(self, x):
+        """
+        Forward pass of the tanh layer.
+        
+        Parameters:
+        ----------
+        x: np.ndarray
+            The input data. The shape of x is (batch_size, input_size)
+        """
+        self.x = np.tanh(x)
+        return self.x
+
+    def backward(self, grad):
+        """
+        Backward pass of the tanh layer.
+        
+        Parameters:
+        ----------
+        grad: np.ndarray
+            The gradient of the loss with respect to the output of the tanh layer. The shape of x is (batch_size, input_size)
+        """
+        return grad * (1 - self.x ** 2)
 
 class ReLU(Layer):
     def __init__(self):
@@ -173,10 +160,7 @@ class ReLU(Layer):
         grad: np.ndarray
             The gradient of the loss with respect to the output of the ReLU layer. The shape of x is (batch_size, input_size)
         """
-        grad_x = grad * (self.x > 0)
-        return grad_x
-
-
+        return grad * (self.x > 0)
 
 class BceLoss(Layer):
     def __init__(self, epsilon=1e-8):
@@ -208,7 +192,7 @@ class BceLoss(Layer):
         self.y = y
         self.batch_size = y_pred.shape[0]
 
-        # clip the predictions to avoid log(0)
+        # clip the predictions numerical stability
         self.y_pred_clip = np.clip(y_pred, self.epsilon, 1 - self.epsilon)
 
         loss_per_sample = -(self.y * np.log(self.y_pred_clip) + (1 - self.y) * np.log(1 - self.y_pred_clip))
@@ -309,85 +293,3 @@ class Sequential(Layer):
                 layer.weights = loaded[f'layer_{idx}_weights']
                 layer.bias = loaded[f'layer_{idx}_bias']
         print(f"Weights loaded from {filepath}")
-    
-# # Test Linear Layer
-# linear_layer = Linear(input_size=3, output_size=2)
-# x = np.array([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]])
-# output = linear_layer.forward(x)
-# grad_z = np.array([[1.0, 2.0], [3.0, 4.0]])
-# grad_x = linear_layer.backward(grad_z)
-
-# print("Forward Output:", output)
-# print("Input Gradient:", grad_x)
-# print("Weights Gradient:", linear_layer.grad_weights)
-# print("Bias Gradient:", linear_layer.grad_bias)
-
-# # Test Sigmoid Layer
-# sigmoid = Sigmoid()
-# x_sigmoid = np.array([[0.5, -1.0], [2.0, 0.0]])
-# output_sigmoid = sigmoid.forward(x_sigmoid)
-# grad_sigmoid = np.array([[0.1, 0.2], [0.3, 0.4]])
-# dx_sigmoid = sigmoid.backward(grad_sigmoid)
-
-# print("\nSigmoid Output:", output_sigmoid)
-# print("Sigmoid Input Gradient:", dx_sigmoid)
-
-# # Test ReLU Layer
-# relu = ReLU()
-# x_relu = np.array([[2.0, -1.0], [0.0, 3.0]])
-# output_relu = relu.forward(x_relu)
-# print("\nReLU Output:", output_relu)
-
-# grad_relu = np.array([[1.0, 2.0], [3.0, 4.0]])
-# dx_relu = relu.backward(grad_relu)
-# print("ReLU Input Gradient:", dx_relu)
-
-
-# Testing xor with neural network
-np.random.seed(42)
-
-X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]], dtype=np.float32)
-y = np.array([[0], [1], [1], [0]], dtype=np.float32)
-
-# model_sigmoid = Sequential()
-# model_sigmoid.add(Linear(input_size=2, output_size=2))
-# model_sigmoid.add(Sigmoid())
-# model_sigmoid.add(Linear(input_size=2, output_size=1))
-# model_sigmoid.add(Sigmoid())
-model_sigmoid = Sequential(
-    layers=[
-        Linear(input_size=2, output_size=2),
-        Sigmoid(),
-        Linear(input_size=2, output_size=1),
-        Sigmoid()
-    ]
-)
-loss_fn = BceLoss()
-learning_rate = 1.0
-n_epochs = 10000
-
-for epoch in range(n_epochs):
-    # Forward pass
-    y_pred = model_sigmoid.forward(X)
-    loss = loss_fn.forward(y_pred, y)
-
-    # Backward pass
-    grad = loss_fn.backward()
-    model_sigmoid.backward(grad)
-
-    # Update weights
-    for layer in model_sigmoid.layers:
-        if isinstance(layer, Linear):
-            layer.weights -= learning_rate * layer.grad_weights
-            layer.bias -= learning_rate * layer.grad_bias
-
-    if epoch % 1000 == 0:
-        print(f"sigmoid loss at epoch {epoch}: {loss:.6f}")
-
-print("\nPredictions after training:")
-print(np.round(model_sigmoid.forward(X), 3))
-
-print('ground truth:')
-print(y)
-
-model_sigmoid.save("XOR_solved_model_sigmoid.npz")
--- xor_problem.py
+++ xor_problem.py
@@ -30,36 +30,69 @@ model_sigmoid = nnl.Sequential(
     ]
 )
 
+model_tanh = nnl.Sequential(
+    layers=[
+        nnl.Linear(input_size = 2, output_size=2),
+        nnl.Tanh(),
+        nnl.Linear(input_size = 2, output_size = 1),
+        nnl.Sigmoid()
+    ]
+)
+
 # Define the loss function
-loss_fn = nnl.BceLoss()
-learning_rate = 0.1
+loss_sig_fn = nnl.BceLoss()
+loss_tanh_fn = nnl.BceLoss()
+learning_rate_sigmoid = 1
+learning_rate_tanh = 0.5
 n_epochs = 100000
 
-# Train the model
+# Train the model_sigmoid
 for epoch in range(n_epochs):
     # Forward pass
-    y_pred = model_sigmoid.forward(X)
-    loss = loss_fn.forward(y_pred, y)
+    y_pred_sig = model_sigmoid.forward(X)
+    loss_sig = loss_sig_fn.forward(y_pred_sig, y)
     
     # Backward pass
-    grad = loss_fn.backward()
-    model_sigmoid.backward(grad)
-        
+    grad_sig = loss_sig_fn.backward()
+    model_sigmoid.backward(grad_sig)
+
     # Update the weights
     for layer in model_sigmoid.layers:
         if isinstance(layer, nnl.Linear):
-            layer.weights -= learning_rate * layer.grad_weights
-            layer.bias -= learning_rate * layer.grad_bias
-
+            layer.weights -= learning_rate_sigmoid * layer.grad_weights
+            layer.bias -= learning_rate_sigmoid * layer.grad_bias
+    
     if epoch % 1000 == 0:
-        print(f"Epoch {epoch}, loss: {loss}")
+        print(f"Epoch {epoch}, Sigmoid loss: {loss_sig}")
 
-print("\nPredictions after training:")
-print(np.round(model_sigmoid.forward(X), 1))
+# Train the model_tanh
+for epoch in range(n_epochs):
+    # Forward pass
+    y_pred_tanh = model_tanh.forward(X)
+    loss_tanh = loss_tanh_fn.forward(y_pred_tanh, y)
+    
+    # Backward pass
+    grad_tanh = loss_tanh_fn.backward()
+    model_tanh.backward(grad_tanh)
+
+    # Update the weights
+    for layer in model_tanh.layers:
+        if isinstance(layer, nnl.Linear):
+            layer.weights -= learning_rate_tanh * layer.grad_weights
+            layer.bias -= learning_rate_tanh * layer.grad_bias
+    
+    if epoch % 1000 == 0:
+        print(f"Epoch {epoch}, Tanh loss: {loss_tanh}")
+
+print("\nPredictions after training for sigmoid:")
+print(np.round(model_sigmoid.forward(X), 3))
 model_sigmoid.save("xor_model_sigmoid.npz")
-# for i in range(X.shape[0]):
-#     x = X[i].reshape(1, -1)
-#     target = y[i].reshape(1, -1)
-#     output = model_sigmoid.forward(x)
-#     print(f"XOR({x}) = {output}")
+print('weights sigmoid:', model_sigmoid.layers[0].weights)
 
+print("\nPredictions after training for tanh:")
+print(np.round(model_tanh.forward(X), 3))
+model_tanh.save("xor_model_tanh.npz")
+print('weights tanh:', model_tanh.layers[0].weights)
+
+print("\nGround truth:")
+print(y)
